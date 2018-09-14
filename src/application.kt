ae6cacf55f467dd9bb07a13f45b25c56b5f563c9
Julien Terrier <legzomil@gmail.com>
2018-09-14 21:54:32 +0200

    Adding some DB stuff with Nitrite

--- src/application.kt
+++ src/application.kt
@@ -1,6 +1,10 @@
 package com.orange.ccmd.sandbox
 
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
@@ -8,6 +12,7 @@ import io.ktor.client.request.get
 import io.ktor.client.request.url
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.pipeline.PipelineContext
 import io.ktor.request.path
@@ -15,9 +20,13 @@ import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.coroutines.experimental.async
+import org.dizitart.kno2.filters.eq
+import org.dizitart.kno2.getRepository
+import org.dizitart.kno2.nitrite
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.event.Level
+import java.io.File
 
 val LOGGER: Logger = LoggerFactory.getLogger("MyLogger")
 const val ROOT_URL = "https://www.strava.com/api/v3/"
@@ -51,10 +60,24 @@ fun Application.module() {
                 url("$ROOT_URL/activities?per_page=$PER_PAGE&access_token=${tokenFromConf()}")
             }
 
-            LOGGER.info("${activities.size} activites found, displaying first ten")
+            LOGGER.info("${activities.size} activites found, returning first ten")
 
-            val firstTenActivities = activities.subList(0 , 10)
-            call.respond(firstTenActivities)
+            val repository = db.getRepository<Activity> {
+                insert(activities.toTypedArray())
+            }
+
+            call.respond(activities.subList(0, 10))
+        }
+
+        get("/db/activities") {
+            LOGGER.info("Getting activities from db")
+
+            val repository = db.getRepository<Activity>()
+            val activities = repository.find().toList()
+
+            LOGGER.info("${activities.size} activites found, returning first ten")
+
+            call.respond(activities.subList(0, 10))
         }
 
         get("/activities/{id}") {
@@ -67,17 +90,42 @@ fun Application.module() {
             call.respond(activity)
         }
 
+        get("/db/activities/{id}") {
+            val id = call.parameters["id"]
+            LOGGER.info("Getting activity with id = $id from db")
+
+            val repository = db.getRepository<Activity>()
+            val activity = repository.find(Activity::id eq id).firstOrNull()
+
+            if (activity != null) {
+                LOGGER.info("Activity found, returning it")
+                call.respond(activity)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+
+        }
+
         get("/activities/all/{ids}") {
 
             val ids = call.parameters["ids"]
             LOGGER.info("Getting activities with ids = $ids")
 
             val activityIds = ids.orEmpty().split(",")
-            val tasks = activityIds.map { activityId -> async { getActivity(client, activityId) }}
+            val tasks = activityIds.map { activityId -> async { getActivity(client, activityId) } }
             val allActivities = tasks.forEach { task -> task.await() }
 
             call.respond(allActivities)
         }
+    }
+}
+
+val db by lazy {
+    nitrite {
+        file = File("resources/strava.db")
+        autoCommitBufferSize = 2048
+        compress = true
+        autoCompact = false
     }
 }
 
