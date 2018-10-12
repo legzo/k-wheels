019c0a9025337491f6b3c24017f989e9d9fe368f
Julien Terrier <legzomil@gmail.com>
2018-10-12 23:41:31 +0200

    Adds ktlint and formats code

--- src/models.kt
+++
@@ -1,8 +0,0 @@
-package com.orange.ccmd.sandbox
-
-import com.google.gson.annotations.SerializedName
-
-data class Activity(val id: String,
-                    val name: String,
-                    val distance: Number,
-                    @SerializedName("moving_time") val movingTime: Number)
